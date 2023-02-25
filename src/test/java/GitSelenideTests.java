b3f6d6dc9699339e5c65873b55d07ade05d7afc7
eka <katrinmadness17@gmail.com>
2023-02-25 23:08:09 +0300

    Added hoverTest and dragAndDropTest

--- src/test/java/GitSelenideTests.java
+++ src/test/java/GitSelenideTests.java
@@ -2,23 +2,43 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class GitSelenideTests {
 
   @BeforeAll
-  static  void beforeAll() {
+  static void beforeAll() {
     Configuration.browserSize = "1920x1080";
-    Configuration.holdBrowserOpen = true;}
+    Configuration.holdBrowserOpen = true;
+  }
   @Test
-  void searchJUnit5Example(){
+  void searchJUnit5Example() {
     open("https://github.com/selenide/selenide");
     $("#wiki-tab").click();
     $(".Box-row.wiki-more-pages-link").$("button").click();
     $(byText("SoftAssertions")).click();
     $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
   }
-}
+
+  @Test
+  void hoverTest() {
+
+    open("https://github.com");
+    $(withText("Solutions")).hover();
+    $(withTagAndText("a", "Enterprise")).click();
+    $(".enterprise-hero").shouldHave(text("Build like the best"));
+
+  }
+
+  @Test
+  void dragAndDropTest() {
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+      }
+    }
 
